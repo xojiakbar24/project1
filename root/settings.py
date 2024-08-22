59b05925741de63c6ac4bae176e19dda5f9aa9f1
xojiakbar24 <xojakbarxaydarovy@gmail.com>
2024-08-22 18:06:55 +0500

    ok

--- root/settings.py
+++ root/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/"apps/static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URl = BASE_DIR
+STATIC_ROOT = BASE_DIR / 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 
 
--- root/urls.py
+++ root/urls.py
@@ -1,19 +1,5 @@
-"""
-URL configuration for root project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from root import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from apps.views import (IndexView, ShopView, AboutView, ServiceView, BlogView,
@@ -33,4 +19,8 @@ urlpatterns = [
     path('signup',SignUpView.as_view(),name='signup'),
     #  path('add-to-cart/<int:pk>/', add_to_cart, name='add_to_cart'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
